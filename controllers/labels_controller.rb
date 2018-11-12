a9d5a523f9754600cad11aae231b70655225769c
Alex Clements <alexfjclements@gmail.com>
2018-11-12 16:00:44 +0000

    new labels adding and saving correctly from webpage

--- controllers/labels_controller.rb
+++ controllers/labels_controller.rb
@@ -3,3 +3,10 @@ get '/labels' do
   @labels = Label.all()
   erb ( :"labels/addrm" )
 end
+
+post '/labels' do
+  # Hard coding user id as not implementing log-in system
+  params.merge!("user_id" => 1)
+  label = Label.new(params)
+  label.save
+end
