86decf43112b8e96d1d806aca42374766c0a27bb
Alex Clements <alexfjclements@gmail.com>
2018-11-12 10:54:12 +0000

    started work on labels page

--- controllers/labels_controller.rb
+++ controllers/labels_controller.rb
@@ -2,3 +2,8 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative( '../models/label.rb' )
 also_reload( '../models/*' )
+
+get '/labels' do
+  @labels = Label.all()
+  erb ( :"labels/index" )
+end
